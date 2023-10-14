a70b976587b46ff25d4d9ed2e788013362da7a74
Gage_Osu <dannilmorozov@gmail.com>
2023-10-14 03:44:53 +0300

    Создание  LambdaCommand

--- ViewModels/Base/ViewModelBase.cs
+++ ViewModels/Base/ViewModelBase.cs
@@ -11,14 +11,14 @@ namespace CourseWork.ViewModels.Base
 {
     internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
 
-        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null)
         {
             if(Equals(field, value)) return false;
             field = value;
