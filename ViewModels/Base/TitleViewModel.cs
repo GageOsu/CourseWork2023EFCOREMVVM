6e0f2e19584d76aeb2fe5fb9d29b0dba49cc6d76
Gage_Osu <dannilmorozov@gmail.com>
2023-10-14 04:11:25 +0300

    Создание TitleViewModel

---
+++ ViewModels/Base/TitleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.ViewModels.Base
+{
+    internal abstract class TitleViewModel : ViewModelBase
+    {
+        #region Заголовок
+        /// <summary>Заголовок</summary>
+        private string _title;
+
+        /// <summary>Заголовок</summary>
+        public string Title { get => _title; set => Set(ref _title, value); }
+        #endregion
+    }
+}
